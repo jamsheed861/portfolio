20e8cd3b331ce2d11c7382719d35bcf7101c609e
zeeshan <47000483+Zeeshan-web-developer@users.noreply.github.com>
2022-03-11 17:59:10 +0530

    Update ProductList.jsx
    added image path

--- src/components/Productlist/ProductList.jsx
+++ src/components/Productlist/ProductList.jsx
@@ -1,32 +1,20 @@
-import React from 'react'
-import "./productlist.css"
-import Product from "../products/Product"
-import  {products} from  "../../data"
+import React from 'react';
+import './productlist.css';
+import Product from '../products/Product';
+import { products } from '../../data';
 const ProductList = () => {
   return (
-    <>
-    <div className='p1'>
-        <div className='p1-texts'>
-
-            
-        <p className='p1-desc'>
-           My Projects
-            </p>
-        
-        
-        </div>
-        <div className='p1-list'>
-          {products.map((item)=>(<Product kry={item.id} img={item.img} link={item.link}/>))}
-
-
-
-
-        </div>
+    <div className="p1">
+      <div className="p1-texts">
+        <p className="p1-desc">My Projects</p>
+      </div>
+      <div className="p1-list">
+        {products.map((item, index) => (
+          <Product key={item.id} image={item.img} link={item.link} />
+        ))}
+      </div>
     </div>
-    
-    
-    </>
-  )
-}
+  );
+};
 
-export default ProductList
+export default ProductList;
